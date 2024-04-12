3bc5e6dec165badb0b17899a891c4615e6f4c59b
robert <robert.allik@protonmail.com>
2024-04-12 18:45:00 +0300

    tried  to change urdf

--- catkin_ws/src/depth_safety_layer/src/nomad_dummy.py
+++ catkin_ws/src/depth_safety_layer/src/nomad_dummy.py
@@ -7,26 +7,26 @@ from geometry_msgs.msg import PoseStamped, Quaternion
 import tf.transformations
 
 def talker():
-  pub = rospy.Publisher("/nomad_dummy", Float32MultiArray, queue_size=1)
+  pub = rospy.Publisher("/nomad_path", Float32MultiArray, queue_size=1)
   goal_pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=1)
   rospy.init_node("nomad_dummy", anonymous=True)
   rate = rospy.Rate(1)
   rospy.loginfo(f"Starting nomad_dummy")
   while not rospy.is_shutdown():
     msg = Float32MultiArray()
-    msg.data = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    msg.data = [0.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     print("sending data", msg.data)
     pub.publish(msg)
-
-    msg2 = PoseStamped()
-    msg2.header.frame_id = "base_link"
-    msg2.header.stamp = rospy.Time.now()
-    msg2.pose.position.x = 1
-    msg2.pose.position.y = 1
-    msg2.pose.orientation.w = 1
+    # msg2 = PoseStamped()
+    # msg2.header.frame_id = "base_link"
+    # msg2.header.stamp = rospy.Time.now()
+    # msg2.pose.position.x = 1
+    # msg2.pose.position.y = 1
+    # msg2.pose.orientation.w = 1
     
-    goal_pub.publish(msg2)
+    # goal_pub.publish(msg2)
     rate.sleep()
+    # rospy.signal_shutdown("end")
   
 
 if __name__ == '__main__':
